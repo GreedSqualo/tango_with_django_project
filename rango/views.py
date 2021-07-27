c32f313cbe9e70b5241a3144fa00503d37ba0651
Chuyan Niu <767713967@qq.com>
2021-07-28 00:23:40 +0800

    chapter4 4.4

--- rango/views.py
+++ rango/views.py
@@ -12,5 +12,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    str2 = "Rango says here is the about page." + "</p>" + "<a href='/rango/'>Index</a>"
-    return HttpResponse(str2)
+    return render(request, 'rango/about.html')
